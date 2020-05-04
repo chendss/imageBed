67389f09e0e7063f7f973512907fdc18547b7ac5
dashao <capslock.ch@outlook.com>
2020-05-04 23:02:59 +0800

    域名

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = window.location.href.includes('localhost') ? 'http://localhost:6388' : ''
+axios.defaults.baseURL = window.location.href.includes('localhost') ? 'http://localhost:6388' : 'https://imgserver.dashao.me:2'
 
 export default {
 
